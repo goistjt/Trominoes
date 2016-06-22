bab778c39b9d145ea2c57ef3ce05c872ac40019b
brownba <brownba1@rose-hulman.edu>
2016-06-21 20:38:54 -0400

    updated to fit deficiency

--- src/src/LRenderer.java
+++ src/src/LRenderer.java
@@ -6,7 +6,7 @@ import java.awt.*;
  * Created by goistjt on 6/20/2016.
  */
 public class LRenderer extends Component {
-    public LRenderer(int n, int[] init_deficiency) {
+    public LRenderer(int n, int init_quadrant_deficiency) {
 
     }
 }
