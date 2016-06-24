2aeb2896dcb173615a6747358f5a8e9b8a00482b
goistjt <goistjt@rose-hulman.edu>
2016-06-23 21:24:38 -0400

    Working on the LRendering

--- src/PySrc/main.py
+++ src/PySrc/main.py
@@ -6,19 +6,6 @@ SIZE = 1000
 color_list = ['red', 'purple', 'yellow', 'green', 'blue', 'cyan', 'orange', 'maroon', 'magenta', 'pink', 'violet']
 
 
-def determine_quadrant(deficiency, n):
-    if deficiency[0] < n / 2:
-        if deficiency[1] < n / 2:
-            return 2
-        else:
-            return 3
-    else:
-        if deficiency[1] < n / 2:
-            return 1
-        else:
-            return 4
-
-
 class QuadrantRenderer(object):
     def __init__(self, n, init_deficiency):
         self.win = GraphWin("Quandrant Renderer", 1000, 1000)
@@ -50,7 +37,6 @@ class QuadrantRenderer(object):
 
     def draw_trominoes(self, deficiency, n, offset_column, offset_row):
         quadrant = determine_quadrant([deficiency[0] - offset_column, deficiency[1] - offset_row], n)
-        print(quadrant)
         if quadrant == 1:
             m1 = [n / 2 - 1 + offset_column, n / 2 + offset_row]
             m2 = [m1[0] + 1, m1[1]]
@@ -113,6 +99,137 @@ class QuadrantRenderer(object):
                             m3[1] * self.grid_size + self.grid_size))
         r.setFill(color)
         r.draw(self.win)
+
+
+class LRenderer(object):
+    def __init__(self, n, init_deficiency):
+        self.win = GraphWin("LShape Renderer", 1000, 1000)
+        self.n = n
+        self.sleep = float(input("How long between drawing of trominoes (in seconds)?: "))
+        self.init_deficiency = init_deficiency
+        self.grid_size = int(1000 / n)
+
+    def render(self):
+        self.draw_init_grid()
+        input("Press Enter to Begin Tiling")
+        self.draw_quadrant(self.init_deficiency, 0, [0, 0], self.n)
+        input("Press Enter to Close")
+        self.win.close()
+
+    def draw_init_grid(self):
+        for x in range(0, self.n):
+            for y in range(0, self.n):
+                if self.init_deficiency[0] == x and self.init_deficiency[1] == y:
+                    r = Rectangle(Point(x * self.grid_size, y * self.grid_size),
+                                  Point(x * self.grid_size + self.grid_size,
+                                        y * self.grid_size + self.grid_size))
+                    r.setFill(color="black")
+                else:
+                    r = Rectangle(Point(x * self.grid_size, y * self.grid_size),
+                                  Point(x * self.grid_size + self.grid_size,
+                                        y * self.grid_size + self.grid_size))
+                r.draw(self.win)
+
+    def draw_quadrant(self, deficiency, step, top_corner):
+        quadrant = determine_quadrant(deficiency, self.n / pow(2, step))
+        print(quadrant)
+        if self.n / pow(2, step) <= 2:
+            if quadrant == 1:
+                self.draw_tromino(top_corner,
+                                  [top_corner[0], top_corner[1] + 1],
+                                  [top_corner[0] + 1, top_corner[1] + 1])
+                sleep(self.sleep)
+
+            elif quadrant == 2:
+                self.draw_tromino([top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1] + 1],
+                                  [top_corner[0], top_corner[1] + 1])
+                sleep(self.sleep)
+            elif quadrant == 3:
+                self.draw_tromino([top_corner[0], top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1] + 1])
+                sleep(self.sleep)
+            elif quadrant == 4:
+                self.draw_tromino([top_corner[0], top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0], top_corner[1] + 1])
+                sleep(self.sleep)
+        else:
+            self.draw_L(quadrant, step, top_corner)
+            if quadrant == 1:
+                self.draw_quadrant([deficiency[0] - self.n / 2, deficiency[1]], step + 1, [self.n / 2, 0])
+            elif quadrant == 2:
+                self.draw_quadrant([deficiency[0], deficiency[1]], step + 1, top_corner)
+            elif quadrant == 3:
+                self.draw_quadrant([deficiency[0], deficiency[1] - self.n / 2], step + 1, [0, self.n / 2])
+            elif quadrant == 4:
+                self.draw_quadrant([deficiency[0] - self.n / 2, deficiency[1] - self.n / 2], step + 1, [self.n / 2,
+                                                                                                        self.n / 2])
+
+    def draw_L(self, quadrant, step, top_corner):
+        if self.n / pow(2, step) <= 2:
+            if quadrant == 1:
+                self.draw_tromino(top_corner,
+                                  [top_corner[0], top_corner[1] + 1],
+                                  [top_corner[0] + 1, top_corner[1] + 1])
+                sleep(self.sleep)
+
+            elif quadrant == 2:
+                self.draw_tromino([top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1] + 1],
+                                  [top_corner[0], top_corner[1] + 1])
+                sleep(self.sleep)
+            elif quadrant == 3:
+                self.draw_tromino([top_corner[0], top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1] + 1])
+                sleep(self.sleep)
+            elif quadrant == 4:
+                self.draw_tromino([top_corner[0], top_corner[1]],
+                                  [top_corner[0] + 1, top_corner[1]],
+                                  [top_corner[0], top_corner[1] + 1])
+                sleep(self.sleep)
+        else:
+            if quadrant == 1:
+                pass
+            elif quadrant == 2:
+                pass
+            elif quadrant == 3:
+                pass
+            elif quadrant == 4:
+                pass
+
+    def draw_tromino(self, m1, m2, m3):
+        color = color_list[randint(0, len(color_list) - 1)]
+        r = Rectangle(Point(m1[0] * self.grid_size, m1[1] * self.grid_size),
+                      Point(m1[0] * self.grid_size + self.grid_size,
+                            m1[1] * self.grid_size + self.grid_size))
+        r.setFill(color)
+        r.draw(self.win)
+        r = Rectangle(Point(m2[0] * self.grid_size, m2[1] * self.grid_size),
+                      Point(m2[0] * self.grid_size + self.grid_size,
+                            m2[1] * self.grid_size + self.grid_size))
+        r.setFill(color)
+        r.draw(self.win)
+        r = Rectangle(Point(m3[0] * self.grid_size, m3[1] * self.grid_size),
+                      Point(m3[0] * self.grid_size + self.grid_size,
+                            m3[1] * self.grid_size + self.grid_size))
+        r.setFill(color)
+        r.draw(self.win)
+
+
+def determine_quadrant(deficiency, n):
+    if deficiency[0] < n / 2:
+        if deficiency[1] < n / 2:
+            return 2
+        else:
+            return 3
+    else:
+        if deficiency[1] < n / 2:
+            return 1
+        else:
+            return 4
 
 
 def main():
